56846cf8b4355c10a692007b59d8d8f280fb16e4
Mario Alberto Chavez <mario.chavez@gmail.com>
2011-01-07 21:06:22 -0800

    Esqueleto inicial del proyecto y primer escenario

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Empleosti::Application.config.secret_token = '12aaa1bffb40735d2f8b6eedc80040f772b9b82dbc2d752e2dd28faf4c4211991021d581004b04fba427be8d725b74e443e8abaabdce8f564a5e231e64a40077'
---
+++ spec/acceptance/publicar_nuevas_ofertas_de_empleo_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
+
+feature "Publicar Nuevas Ofertas De Empleo", %q{
+  Para poder llenar una posicion de trabajo
+  Como empresa interesada
+  Debo poder poder publicar una oferta
+} do
+
+  scenario "Scenario name" do
+    true.should == true
+  end
+end
