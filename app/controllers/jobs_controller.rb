70ec971b2abdfbb9c817d377575abd8d8058a907
Mario Alberto Chavez <mario.chavez@gmail.com>
2011-01-08 15:14:07 -0800

    Se completo el segundo escenario

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,4 +3,14 @@ class JobsController < ApplicationController
     @job = Job.new
   end
 
+  def create
+    @job = Job.new params[:job]
+
+    if @job.save
+      redirect_to root_path, :notice => 'La oferta ha sido creada'
+    else
+      render :new
+    end
+  end
+
 end
--- spec/acceptance/support/paths.rb
+++ spec/acceptance/support/paths.rb
@@ -4,6 +4,10 @@ module NavigationHelpers
   def homepage
     "/"
   end
+
+  def new_job
+    "/jobs/new"
+  end
 end
 
 RSpec.configuration.include NavigationHelpers, :type => :acceptance
